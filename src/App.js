c9445eb4e8a6b4924d00be83fda0bf9320881dff
Arik Alexandrov <arikxl@gmail.com>
2022-04-11 17:50:35 +0300

    router and sidebar

--- src/App.js
+++ src/App.js
@@ -1,11 +1,36 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+
 import './App.css';
+import AppHeader from "./components/app-header/AppHeader";
+import AppSidebar from "./components/app-sidebar/AppSidebar";
+import Orders from "./components/orders/Orders";
+import Products from "./components/products/Products";
+import Users from "./components/users/Users";
+import CategoriesPage from "./pages/categories-page/CategoriesPage";
+
 import HomePage from './pages/home-page/HomePage';
 
 function App() {
   return (
-    <div className="App">
-      <HomePage />
-    </div>
+    <BrowserRouter>
+      <div className="flex">
+      <AppSidebar />
+
+      <div className="flex column">
+        <AppHeader />
+        
+        <hr />
+        <Routes>
+          <Route path="/" element={<HomePage />}/>
+          <Route path="/categories" element={<CategoriesPage/>} />
+          <Route path="/orders" element={<Orders/>} />
+          <Route path="/products" element={<Products/>} />
+          <Route path="/users" element={<Users/>} />
+        </Routes>
+      </div>
+      </div>
+    </BrowserRouter>
   );
 }
 
--- src/components/app-sidebar/AppSidebar.jsx
+++ src/components/app-sidebar/AppSidebar.jsx
@@ -1,8 +1,40 @@
-import React from 'react'
+import React from 'react';
+import styled from 'styled-components';
+import { Link } from 'react-router-dom';
+
+
+
+const SidebarStyled = styled.nav`
+  width:25% ;
+  height: 100vh;
+  border: 2px solid chocolate ;
+`;
 
 const AppSidebar = () => {
   return (
-    <div>AppSidebar</div>
+
+    <SidebarStyled>
+      <div>
+        <Link to='/'>
+          LOGO
+        </Link>
+        &nbsp;
+        <button>🔘</button>
+      </div>
+      <hr />
+      <Link to='/categories'>
+        <h1>Categories</h1>
+      </Link>
+      <Link to='/orders'>
+        <h1>Orders</h1>
+      </Link>
+      <Link to='/products'>
+        <h1>Products</h1>
+      </Link>
+      <Link to='/users'>
+        <h1>Users</h1>
+      </Link>
+    </SidebarStyled>
   )
 }
 
